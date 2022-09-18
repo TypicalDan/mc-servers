6a1ce76bd162532c60eb0e7c78596de4a88d4a55
TypicalDan <db5000db.34@gmail.com>
2022-09-18 21:52:36 +0100

    Fixed shite

--- update-mc.sh
+++ update-mc.sh
@@ -33,7 +33,7 @@ waterfallName=$(sudo ls /home/minecraft/mc-servers/jars/waterfall)
 echo "New waterfall version is $waterfallName"
 
 echo "Finding purpur .jar..."
-purpurName=$(sudo ls /home/minecraft/mc-servers/purpur)
+purpurName=$(sudo ls /home/minecraft/mc-servers/jars/purpur)
 echo "New purpur version is $purpurName"
 
 # Showing new plugins
@@ -52,7 +52,7 @@ sudo ls /home/minecraft/mc-servers/datapacks
 echo "Moving .jar files..."
 sudo mv /home/minecraft/mc-servers/jars/waterfall/$waterfallName /home/minecraft/servers/Purpurfall_V6/Waterfall/waterfall.jar
 echo "Moved waterfall .jar!"
-sudo cp /home/minecraft/mc-servers/jars/purpur/$purpurName /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/purpur.jar
+sudo cp -r /home/minecraft/mc-servers/jars/purpur/$purpurName /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/purpur.jar
 echo "Moved survival .jar!"
 sudo mv /home/minecraft/mc-servers/jars/purpur/$purpurName /home/minecraft/servers/Purpurfall_V6/PurpurCreative/purpur.jar
 echo "Moved creative .jar!"
@@ -67,7 +67,7 @@ sudo mv /home/minecraft/mc-servers/plugins/creative /home/minecraft/servers/Purp
 
 # Moving datapacks across
 echo "Moving datapacks..."
-sudo cp /home/minecraft/mc-servers/datapacks/survival /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/Bruhhh/datapacks
+sudo cp -r /home/minecraft/mc-servers/datapacks/survival /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/Bruhhh/datapacks
 echo "Moved survival datapacks!"
 sudo mv /home/minecraft/mc-servers/datapacks/creative /home/minecraft/servers/Purpurfall_V6/PurpurCreative/Bruhhh2/datapacks
 echo "Moved creative datapacks!"
