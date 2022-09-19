9b3d9aa2ffddfb75dfe93b841cf09f8e69ea71f6
TypicalDan <db5000db.34@gmail.com>
2022-09-19 14:58:05 +0100

    Sorted Perms
    Removed the sudo clauses that aren't needed anymore

--- update-mc.sh
+++ update-mc.sh
@@ -8,76 +8,77 @@ echo "Finished system updates!"
 
 # Old file removal
 echo "Removing old files..."
-sudo rm -r /home/minecraft/mc-servers
+rm -r /home/minecraft/mc-servers
 echo "Cleared repository destination!"
-sudo rm /home/minecraft/servers/Purpurfall_V6/Waterfall/waterfall.jar
-sudo rm /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/purpur.jar
-sudo rm /home/minecraft/servers/Purpurfall_V6/PurpurCreative/purpur.jar
+rm /home/minecraft/servers/Purpurfall_V6/Waterfall/waterfall.jar
+rm /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/purpur.jar
+rm /home/minecraft/servers/Purpurfall_V6/PurpurCreative/purpur.jar
 echo "Cleared old jars!"
-sudo rm /home/minecraft/servers/Purpurfall_V6/Waterfall/plugins/*.jar
-sudo rm /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/plugins/*.jar
-sudo rm /home/minecraft/servers/Purpurfall_V6/PurpurCreative/plugins/*.jar
+rm /home/minecraft/servers/Purpurfall_V6/Waterfall/plugins/*.jar
+rm /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/plugins/*.jar
+rm /home/minecraft/servers/Purpurfall_V6/PurpurCreative/plugins/*.jar
 echo "Cleared old plugins!"
-sudo rm /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/Bruhhh/datapacks/*.zip
-sudo rm /home/minecraft/servers/Purpurfall_V6/PurpurCreative/Bruhhh2/datapacks/*.zip
+rm /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/Bruhhh/datapacks/*.zip
+rm /home/minecraft/servers/Purpurfall_V6/PurpurCreative/Bruhhh2/datapacks/*.zip
 
 # Github cloning
 echo "Fetching new files..."
-sudo git clone https://github.com/TypicalDan/mc-servers /home/minecraft/mc-servers
-sudo chown -R minecraft /home/minecraft/mc-servers
+git clone https://github.com/TypicalDan/mc-servers /home/minecraft/mc-servers
+chown -R minecraft /home/minecraft/mc-servers
 echo "Finished downloading!"
 
 # Finding .jar names
 echo "Finding waterfall .jar..."
-waterfallName=$(sudo ls /home/minecraft/mc-servers/jars/waterfall)
+waterfallName=$(ls /home/minecraft/mc-servers/jars/waterfall)
 echo "New waterfall version is $waterfallName"
 
 echo "Finding purpur .jar..."
-purpurName=$(sudo ls /home/minecraft/mc-servers/jars/purpur)
+purpurName=$(ls /home/minecraft/mc-servers/jars/purpur)
 echo "New purpur version is $purpurName"
 
 # Showing new plugins
 echo "Here are the new plugins:"
 echo "For the waterfall server:"
-sudo ls /home/minecraft/mc-servers/plugins/waterfall
+ls /home/minecraft/mc-servers/plugins/waterfall
 echo "For the survival server:"
-sudo ls /home/minecraft/mc-servers/plugins/survival
+ls /home/minecraft/mc-servers/plugins/survival
 echo "For the creative server:"
-sudo ls /home/minecraft/mc-servers/plugins/creative
+ls /home/minecraft/mc-servers/plugins/creative
 
 echo "Here are the new datapacks:"
-sudo ls /home/minecraft/mc-servers/datapacks
+ls /home/minecraft/mc-servers/datapacks
 
 # Moving .jar files
 echo "Moving .jar files..."
-sudo mv /home/minecraft/mc-servers/jars/waterfall/$waterfallName /home/minecraft/servers/Purpurfall_V6/Waterfall/waterfall.jar
+mv /home/minecraft/mc-servers/jars/waterfall/$waterfallName /home/minecraft/servers/Purpurfall_V6/Waterfall/waterfall.jar
 echo "Moved waterfall .jar!"
-sudo cp /home/minecraft/mc-servers/jars/purpur/$purpurName /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/purpur.jar
+cp /home/minecraft/mc-servers/jars/purpur/$purpurName /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/purpur.jar
 echo "Moved survival .jar!"
-sudo mv /home/minecraft/mc-servers/jars/purpur/$purpurName /home/minecraft/servers/Purpurfall_V6/PurpurCreative/purpur.jar
+mv /home/minecraft/mc-servers/jars/purpur/$purpurName /home/minecraft/servers/Purpurfall_V6/PurpurCreative/purpur.jar
 echo "Moved creative .jar!"
 
 # Moving plugins across
 echo "Moving plugins..."
-sudo mv /home/minecraft/mc-servers/plugins/waterfall /home/minecraft/servers/Purpurfall_V6/Waterfall/plugins
+mv /home/minecraft/mc-servers/plugins/waterfall /home/minecraft/servers/Purpurfall_V6/Waterfall/plugins
 echo "Moved waterfall plugins!"
-sudo mv /home/minecraft/mc-servers/plugins/survival /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/plugins
+mv /home/minecraft/mc-servers/plugins/survival /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/plugins
 echo "Moves survival plugins!"
-sudo mv /home/minecraft/mc-servers/plugins/creative /home/minecraft/servers/Purpurfall_V6/PurpurCreative/plugins
+mv /home/minecraft/mc-servers/plugins/creative /home/minecraft/servers/Purpurfall_V6/PurpurCreative/plugins
 
 # Moving datapacks across
 echo "Moving datapacks..."
-sudo cp -r /home/minecraft/mc-servers/datapacks/survival /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/Bruhhh/datapacks
+cp -r /home/minecraft/mc-servers/datapacks/survival /home/minecraft/servers/Purpurfall_V6/PurpurSurvival/Bruhhh/datapacks
 echo "Moved survival datapacks!"
-sudo mv /home/minecraft/mc-servers/datapacks/creative /home/minecraft/servers/Purpurfall_V6/PurpurCreative/Bruhhh2/datapacks
+mv /home/minecraft/mc-servers/datapacks/creative /home/minecraft/servers/Purpurfall_V6/PurpurCreative/Bruhhh2/datapacks
 echo "Moved creative datapacks!"
 
 # Moving .sh files to the proper places
-sudo mv /home/minecraft/mc-servers/startCreative.sh /home/minecraft/startCreative.sh
-sudo mv /home/minecraft/mc-servers/startSurvival.sh /home/minecraftstartSurvival.sh
-sudo mv /home/minecraft/mc-servers/startWaterfall.sh /home/minecraftstartWaterfall.sh
+mv /home/minecraft/mc-servers/startCreative.sh /home/minecraft/startCreative.sh
+mv /home/minecraft/mc-servers/startSurvival.sh /home/minecraftstartSurvival.sh
+mv /home/minecraft/mc-servers/startWaterfall.sh /home/minecraftstartWaterfall.sh
 
 echo "Sorting permissions..."
-sudo chown -R minecraft /home/minecraft/servers/Purpurfall_V6
+sudo chown -R minecraft /home/minecraft
+sudo chown -R dan /home/minecraft
 
 echo "All done!"
