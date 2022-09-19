47f0a8d466149e13148435f7b48e53978d2c0bda
TypicalDan <db5000db.34@gmail.com>
2022-09-19 15:05:38 +0100

    Removed sudo
    No need to sudo when there's no reason to, right?

--- updateScript.sh
+++ updateScript.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo "Removing old files..."
-sudo rm -r /home/minecraft/mc-servers
+rm -r /home/minecraft/mc-servers
 
 echo "Fetching new files..."
-sudo git clone https://github.com/TypicalDan/mc-servers /home/minecraft/mc-servers
+git clone https://github.com/TypicalDan/mc-servers /home/minecraft/mc-servers
 sudo chown -R minecraft /home/minecraft/mc-servers
 sudo chown -R dan /home/minecraft/mc-servers
 echo "Finished downloading!"
 
 echo "Moving script..."
-sudo mv /home/minecraft/mc-servers/update-mc.sh update-mc.sh
+mv /home/minecraft/mc-servers/update-mc.sh update-mc.sh
 echo "All done!"
